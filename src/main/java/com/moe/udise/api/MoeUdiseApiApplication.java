3d2387d971c87636a56a76fdbea904537cba2216
shamim <shamimahmad586@gmail.com>
2022-11-05 22:18:23 +0530

    This is my first commit

---
+++ src/main/java/com/moe/udise/api/MoeUdiseApiApplication.java
@@ -0,0 +1,13 @@
+package com.moe.udise.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoeUdiseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoeUdiseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moe/udise/api/MoeUdiseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moe.udise.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoeUdiseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
